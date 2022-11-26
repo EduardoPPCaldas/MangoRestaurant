ccb3430a387bcc40272fcf009b5c43c97c3f7199
EduardoPPCaldas <eduardocaldas.dev@gmail.com>
2022-11-26 03:28:30 -0300

    Fill IProductRepository

--- Services/Mango.Services.ProductAPI/Repositories/IProductRepository.cs
+++ Services/Mango.Services.ProductAPI/Repositories/IProductRepository.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mango.Services.ProductAPI.Models.DTOs;
 
 namespace Mango.Services.ProductAPI.Repositories;
 
 public interface IProductRepository
 {
-    
+    Task<IEnumerable<ProductDTO>> GetProducts();
+    Task<ProductDTO> GetProductById(int productId);
+    Task<ProductDTO> CreateProduct(ProductDTO productDTO);
+    Task<ProductDTO> UpdateProduct(ProductDTO productDTO);
+    Task<bool> DeleteProduct(int productId);
 }
